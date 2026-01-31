ddd250ec93e701f5806a40ed709d62e742a322fa
abominablepug <esnahoum@gmail.com>
2026-01-31 18:39:56 -0500

    random letter generation

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
+import random
+import string
 from fastapi import FastAPI
 
 app = FastAPI()
 
+def generate_random_letter():
+    letter = random.choice(string.ascii_letters)
+    return letter
+
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    letters = ""
+    for i in range(100):
+        letters += generate_random_letter()
+    return {"random_letters": letters}
