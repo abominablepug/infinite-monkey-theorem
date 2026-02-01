b3a4a86312e20e6ae615fa3a7c01328d1bd3459f
abominablepug <esnahoum@gmail.com>
2026-01-31 20:21:40 -0500

    frontend added

---
+++ frontend/src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import './global.css';
+
+	let { children } = $props();
+</script>
+
+<svelte:head></svelte:head>
+{@render children()}
---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<h1 class="text-5xl">Infinite Monkey Theorem</h1>
+<br class="my-4" />
+<blockquote class="italic border-l-4 pl-4 border-gray-400 text-xl" cite="https://en.wikipedia.org/wiki/Infinite_monkey_theorem">
+	The infinite monkey theorem states that a monkey hitting keys independently and at random on a typewriter keyboard for an infinite amount of time will almost surely type any given text, including the complete works of William Shakespeare.
+</blockquote>
