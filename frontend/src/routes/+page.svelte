f73544ae29dc70526f73f1a934378c38948bba93
abominablepug <esnahoum@gmail.com>
2026-01-31 21:51:21 -0500

    websocket frontend connection

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,5 +1,37 @@
-<h1 class="text-5xl">Infinite Monkey Theorem</h1>
-<br class="my-4" />
-<blockquote class="italic border-l-4 pl-4 border-gray-400 text-xl" cite="https://en.wikipedia.org/wiki/Infinite_monkey_theorem">
-	The infinite monkey theorem states that a monkey hitting keys independently and at random on a typewriter keyboard for an infinite amount of time will almost surely type any given text, including the complete works of William Shakespeare.
-</blockquote>
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	let socket: WebSocket;
+	let typedText = '';
+	let status = "Disconnected";
+
+	onMount(() => {
+		socket = new WebSocket('ws://localhost:8000/ws');
+
+		socket.onmessage = (event) => {
+			typedText += event.data;
+		};
+
+		socket.onopen = () => {
+			status = "Connected";
+		};
+		socket.onclose = () => {
+			status = "Disconnected";
+		};
+	});
+
+	const toggle = (cmd: string) => socket.send(cmd);
+</script>
+
+<main>
+	<h1>Infinite Monkey Dashboard</h1>
+	<p>Status: {status}</p>
+
+	<div>
+		<button on:click={() => toggle('start')}>Start Typing</button>
+		<button on:click={() => toggle('stop')}>Stop Typing</button>
+		<button on:click={() => typedText = ""}>Reset</button>
+	</div>
+
+	<div>{typedText}</div>
+</main>
